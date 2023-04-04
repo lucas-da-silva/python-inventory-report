9ec357cf725746a3c4d7e73fbfd6f0621843180a
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-04 17:16:28 -0300

    feat: adds json file reading to import_data method

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -7,9 +8,12 @@ class Inventory:
     @classmethod
     def import_data(cls, path, type):
         with open(path, "r") as file:
-            productsCsv = csv.DictReader(file, delimiter=",")
-            products = [product for product in productsCsv]
+            if path.endswith(".json"):
+                products = json.load(file)
+            elif path.endswith(".csv"):
+                products = csv.DictReader(file, delimiter=",")
+            formatted_products = [product for product in products]
         if type == "simples":
-            return SimpleReport.generate(products)
+            return SimpleReport.generate(formatted_products)
         elif type == "completo":
-            return CompleteReport.generate(products)
+            return CompleteReport.generate(formatted_products)
