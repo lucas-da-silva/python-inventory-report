1ff7bb4e952efb6b1fcd010a99d4f95754072428
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-04 16:20:30 -0300

    feat: create class SimpleReport and method generate

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,29 @@
+from datetime import datetime
+
+
 class SimpleReport:
-    pass
+    @classmethod
+    def generate(cls, list):
+        date_today = datetime.today().strftime("%Y-%m-%d")
+        oldest_manufacturing_date = min(
+            [company["data_de_fabricacao"] for company in list]
+        )
+        nearest_expiration_date = min([
+            company["data_de_validade"]
+            for company in list
+            if company["data_de_validade"] > date_today
+        ])
+        company_count = dict()
+
+        for company in list:
+            if company["nome_da_empresa"] in company_count:
+                company_count[company["nome_da_empresa"]] += 1
+            else:
+                company_count[company["nome_da_empresa"]] = 1
+        company_with_more_products = max(company_count, key=company_count.get)
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
+            f"Data de validade mais próxima: {nearest_expiration_date}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
