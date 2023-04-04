6c7b5ec3745d8bd38b68a9a24b6e8f85cb6d2240
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-04 18:53:52 -0300

    feat: adds xml file reading to import_data method

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,7 @@
 import csv
 import json
+from bs4 import BeautifulSoup
+
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 
@@ -12,6 +14,32 @@ class Inventory:
                 products = json.load(file)
             elif path.endswith(".csv"):
                 products = csv.DictReader(file, delimiter=",")
+            else:
+                products_xml = BeautifulSoup(file, "xml").find_all("record")
+                products = [
+                    {
+                        "id": product.find("id").text,
+                        "nome_do_produto": product.find(
+                            "nome_do_produto"
+                        ).text,
+                        "nome_da_empresa": product.find(
+                            "nome_da_empresa"
+                        ).text,
+                        "data_de_fabricacao": product.find(
+                            "data_de_fabricacao"
+                        ).text,
+                        "data_de_validade": product.find(
+                            "data_de_validade"
+                        ).text,
+                        "numero_de_serie": product.find(
+                            "numero_de_serie"
+                        ).text,
+                        "instrucoes_de_armazenamento": product.find(
+                            "instrucoes_de_armazenamento"
+                        ).text,
+                    }
+                    for product in products_xml
+                ]
             formatted_products = [product for product in products]
         if type == "simples":
             return SimpleReport.generate(formatted_products)
