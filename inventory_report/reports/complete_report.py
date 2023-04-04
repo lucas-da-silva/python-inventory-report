3d3a4de35b25c3dff85529d97b3d29f5a1fedff9
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-04 16:49:10 -0300

    feat: create CompleteReport class and generate method inside it, also rename list to products in simple_report file

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,23 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @classmethod
+    def generate(cls, products):
+        products_by_company = {}
+        products_by_company_str = ""
+        
+        for product in products:
+            if product["nome_da_empresa"] in products_by_company:
+                products_by_company[product["nome_da_empresa"]] += 1
+            else:
+                products_by_company[product["nome_da_empresa"]] = 1
+
+        for company, quantity in products_by_company.items():
+            products_by_company_str += f"- {company}: {quantity}\n"
+
+        return (
+            f"{super().generate(products)}\n"
+            "Produtos estocados por empresa:\n"
+            f"{products_by_company_str}"
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -3,19 +3,19 @@ from datetime import datetime
 
 class SimpleReport:
     @classmethod
-    def generate(cls, list):
+    def generate(cls, products):
         date_today = datetime.today().strftime("%Y-%m-%d")
         oldest_manufacturing_date = min(
-            [company["data_de_fabricacao"] for company in list]
+            [company["data_de_fabricacao"] for company in products]
         )
         nearest_expiration_date = min([
             company["data_de_validade"]
-            for company in list
+            for company in products
             if company["data_de_validade"] > date_today
         ])
         company_count = dict()
 
-        for company in list:
+        for company in products:
             if company["nome_da_empresa"] in company_count:
                 company_count[company["nome_da_empresa"]] += 1
             else:
