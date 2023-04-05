b204d25d969d2636e23abeae25ba056dc6f100f4
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-05 10:17:48 -0300

    refactor: create importer_mapping variable, ReportType Enum and ReportFactory class to refactor Inventory class

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,20 +1,37 @@
+import os
+from enum import Enum
 from inventory_report.reports.complete_report import CompleteReport
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.importer.json_importer import JsonImporter
 from inventory_report.importer.csv_importer import CsvImporter
 from inventory_report.importer.xml_importer import XmlImporter
 
+importer_mapping = {
+    ".json": JsonImporter,
+    ".csv": CsvImporter,
+    ".xml": XmlImporter,
+}
+
+
+class ReportType(Enum):
+    SIMPLE = "simples"
+    COMPLETE = "completo"
+
+
+class ReportFactory:
+    @staticmethod
+    def generate(report_type, inventory_items):
+        if report_type == ReportType.SIMPLE.value:
+            return SimpleReport.generate(inventory_items)
+        elif report_type == ReportType.COMPLETE.value:
+            return CompleteReport.generate(inventory_items)
+
 
 class Inventory:
     @classmethod
-    def import_data(cls, path, type):
-        if path.endswith(".json"):
-            products = JsonImporter.import_data(path)
-        elif path.endswith(".csv"):
-            products = CsvImporter.import_data(path)
-        else:
-            products = XmlImporter.import_data(path)
-        if type == "simples":
-            return SimpleReport.generate(products)
-        else:
-            return CompleteReport.generate(products)
+    def import_data(cls, path, report_type):
+        file_extension = os.path.splitext(path)[1]
+        importer = importer_mapping[file_extension]
+        inventory_items = importer.import_data(path)
+        report = ReportFactory.generate(report_type, inventory_items)
+        return report
