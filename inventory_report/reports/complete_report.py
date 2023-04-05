94a3de2517d4b8c00442d6fab16d36ab8ff3d564
Lucas da Silva <lucassouza00012345@gmail.com>
2023-04-05 20:05:15 -0300

    refactor: applied type hints and refactor list comprehesion in generate method in CompleteReport and SimpleReport classes

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,23 +1,20 @@
+from collections import Counter
 from inventory_report.reports.simple_report import SimpleReport
 
 
 class CompleteReport(SimpleReport):
     @classmethod
-    def generate(cls, products):
-        products_by_company = {}
-        products_by_company_str = ""
-
-        for product in products:
-            if product["nome_da_empresa"] in products_by_company:
-                products_by_company[product["nome_da_empresa"]] += 1
-            else:
-                products_by_company[product["nome_da_empresa"]] = 1
-
-        for company, quantity in products_by_company.items():
-            products_by_company_str += f"- {company}: {quantity}\n"
+    def generate(cls, products: list[dict]) -> str:
+        products_by_company = Counter(
+            product["nome_da_empresa"] for product in products
+        )
+        products_by_company_str = "\n".join(
+            f"- {company}: {quantity}"
+            for company, quantity in products_by_company.items()
+        )
 
         return (
             f"{super().generate(products)}\n"
             "Produtos estocados por empresa:\n"
-            f"{products_by_company_str}"
+            f"{products_by_company_str}\n"
         )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,26 +1,23 @@
+from collections import Counter
 from datetime import datetime
 
 
 class SimpleReport:
     @classmethod
-    def generate(cls, products):
+    def generate(cls, products: list[dict]) -> str:
         date_today = datetime.today().strftime("%Y-%m-%d")
         oldest_manufacturing_date = min(
-            [company["data_de_fabricacao"] for company in products]
+            product["data_de_fabricacao"] for product in products
         )
-        nearest_expiration_date = min([
-            company["data_de_validade"]
-            for company in products
-            if company["data_de_validade"] > date_today
-        ])
-        company_count = dict()
-
-        for company in products:
-            if company["nome_da_empresa"] in company_count:
-                company_count[company["nome_da_empresa"]] += 1
-            else:
-                company_count[company["nome_da_empresa"]] = 1
-        company_with_more_products = max(company_count, key=company_count.get)
+        nearest_expiration_date = min(
+            product["data_de_validade"]
+            for product in products
+            if product["data_de_validade"] > date_today
+        )
+        company_count = Counter(
+            product["nome_da_empresa"] for product in products
+        )
+        company_with_more_products = company_count.most_common(1)[0][0]
 
         return (
             f"Data de fabricação mais antiga: {oldest_manufacturing_date}\n"
